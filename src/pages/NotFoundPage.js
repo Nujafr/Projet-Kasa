23b7b08d613430948c562f744714ca81627ce023
Nujafr <nujapro@gmail.com>
2024-10-21 03:02:44 +0200

    fix: 404 page content

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import '../components/404.css';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page non trouvée</h1>
-      <p>Oups! La page que vous cherchez n'existe pas.</p>
+    <div className="error">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <nav>
+        <ul className="nav-return">
+          <li><Link to="/">Retourner sur la page d'accueil</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 };
